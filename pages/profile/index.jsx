07226aee2dbc776e76dac4f90b1a838075615181
asem <islamasif570@gmail.com>
2023-12-11 18:50:31 +0600

    profile -1

--- pages/profile/index.jsx
+++ pages/profile/index.jsx
@@ -1,6 +1,7 @@
 "use client";
 import React, { useEffect } from "react";
 import { signOut, useSession } from "next-auth/react";
+import useCurrentUser from "../../hooks/useCurrentUser";
 import { useRouter } from "next/navigation";
 const Profile = () => {
   const session = useSession();
@@ -10,15 +11,26 @@ const Profile = () => {
       router.push("/auth");
     }
   }, [session?.status]);
-
+  const { currentUser } = useCurrentUser();
   return (
-    <div>
-      <button
-        className="bg-green-500 h-12 w-full text-white"
-        onClick={() => signOut({ callbackUrl: "/auth" })}
-      >
-        singout
-      </button>
+    <div className="w-full mx-4 my-16 max-w-md flex flex-col gap-4">
+      <div className="w-full flex items-start justify-center">
+        <div className="h-16 w-16 md:h-24 md:w-24 rounded-full overflow-clip border-2 border-neutral-300">
+          <img
+            src={currentUser?.image || "images/Netflix-avatar.png"}
+            className="h-full w-full"
+            alt="logo"
+          />
+        </div>
+        <div className="w-full">
+          <input
+            className="h-6 rounded-md border-neutral-300 outline-none text-white text-lg font-semibold"
+            type="text"
+            value={currentUser?.username || ""}
+            disabled={true}
+          />
+        </div>
+      </div>
     </div>
   );
 };
